b234590f6d977840ea0bc73dd0d7c3674c51f45a
Tihh Gonçalves <tihh.gon@gmail.com>
2014-04-09 09:32:35 -0300

    Adicionadas funções de Codificação de String
    Adicionadas as funções:
    
    - nbrDecode()
    - nbrEncode()

--- cms/functions/nbr.string.php
+++ cms/functions/nbr.string.php
@@ -50,4 +50,21 @@ function textLimit($string, $length, $replacer = '...'){
   return $string;
 }
 
+
+function nbrEncode($string) {
+  $data = base64_encode($string);
+  $data = str_replace(array('+','/','='),array('-','_',''),$data);
+  return $data;
+}
+
+function nbrDecode($string) {
+  $data = str_replace(array('-','_'),array('+','/'),$string);
+  $mod4 = strlen($data) % 4;
+  if ($mod4) {
+    $data .= substr('====', $mod4);
+  }
+  return base64_decode($data);
+}
+
+
 ?>
