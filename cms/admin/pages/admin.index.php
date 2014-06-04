3762ac90df6874912af8d19d1110394c9e157a6c
Tihh Gonçalves <tihh.gon@gmail.com>
2014-06-04 11:25:16 -0300

    A 'index' do Admin agora é apresentada por iFrame

--- cms/admin/pages/admin.index.php
+++ cms/admin/pages/admin.index.php
@@ -1,10 +1,1 @@
-<?
-$handle = fopen("http://releases.novabrazil.art.br/ministrar.index/index.html", "rb");
-$contents = null;
-
-while (!feof($handle)) {
-  $contents .= fread($handle, 8192);
-}
-echo $contents;
-fclose($handle);
-?>
+<iframe src="http://releases.novabrazil.art.br/ministrar.index/index.html"></iframe>
